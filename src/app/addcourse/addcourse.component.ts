3fe4a4e10d6ab99c3bdcaa23b40bd3a788d3ea0d
sankar <sankarsuresh1997@gmail.com>
2022-11-29 11:14:21 +0530

    first

--- src/app/addcourse/addcourse.component.ts
+++ src/app/addcourse/addcourse.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./addcourse.component.css']
 })
 export class AddcourseComponent {
-
+ 
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddcourseComponent } from './addcourse/addcourse.component';
+import { ViewComponent } from './view/view.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import {  RouterModule, Routes } from '@angular/router';
+
+const myRoute:Routes=[{
+  path:"",
+  component:AddcourseComponent
+
+},
+{
+path:"view",
+component:ViewComponent
+}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddcourseComponent
+    AddcourseComponent,
+    ViewComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
